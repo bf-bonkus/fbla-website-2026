28ca256e1cf3efce60a0a41393f02cda23fd26cc
Elijah Styler <elijahstyler@gmail.com>
2026-02-03 20:30:56 -0700

    fblaloney

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,27 @@
-<h1>Welcome to the dark abyss of depression</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentations</p>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>SHS Lost and Found</title>
+    <link rel="stylesheet" href="Header.css">
+    <link rel="stylesheet" href="nav.css">
+    <link rel="stylesheet" href="https://use.typekit.net/ate6hrn.css">
+</head>
+
+<body>
+    <div class="header">
+        <h1>Skyridge High School Lost and Found</h1>
+    </div>
+
+    
+    <div class="navbar" style="text-align: center;">
+        <a href="index.html">Home</a> &nbsp;
+        <a href="report.html">Report Lost Item</a> &nbsp;
+        <a href="view.html">View Found Items</a> &nbsp;
+        <a href="contact.html">Contact Us</a>
+    </div>
+
+    <h1>HOME</h1>
+
+
+
+</body>
