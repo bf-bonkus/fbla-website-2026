1d87e37b9c43765500d0de16267e0c8c2c8995cd
bf-bonkus <bf.bonkus@proton.me>
2026-02-03 20:36:28 -0700

    remove HTML type content

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,27 +1,13 @@
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>SHS Lost and Found</title>
-    <link rel="stylesheet" href="Header.css">
-    <link rel="stylesheet" href="nav.css">
-    <link rel="stylesheet" href="https://use.typekit.net/ate6hrn.css">
-</head>
-
-<body>
-    <div class="header">
-        <h1>Skyridge High School Lost and Found</h1>
-    </div>
-
-    
-    <div class="navbar" style="text-align: center;">
-        <a href="index.html">Home</a> &nbsp;
-        <a href="report.html">Report Lost Item</a> &nbsp;
-        <a href="view.html">View Found Items</a> &nbsp;
-        <a href="contact.html">Contact Us</a>
-    </div>
-
-    <h1>HOME</h1>
+<div class="header">
+    <h1>Skyridge High School Lost and Found</h1>
+</div>
 
 
+<div class="navbar" style="text-align: center;">
+    <a href="index.html">Home</a> &nbsp;
+    <a href="report.html">Report Lost Item</a> &nbsp;
+    <a href="view.html">View Found Items</a> &nbsp;
+    <a href="contact.html">Contact Us</a>
+</div>
 
-</body>
+<h1>HOME</h1>
