055354c7be983c5f29b05bb2ce6df13370381d14
Elijah Styler <elijahstyler@gmail.com>
2026-02-06 11:32:44 -0700

    updated stylesheet

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,12 +3,16 @@
 
 	import Navbar from '$lib/navbar.svelte';
 
+	import style from '$lib/style.css';
+
 	let { children } = $props();
 </script>
 
 <svelte:head>
 	<link rel="icon" href={favicon} />
+	<link rel="stylesheet" href={style} />
 </svelte:head>
+
 
 <Navbar />
 {@render children()}
